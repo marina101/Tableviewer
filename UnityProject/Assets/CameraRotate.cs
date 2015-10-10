37b4854928fde98c2c06674ebc6dc1f38c4de1fa
Anthony <fioritoanthony@outlook.com>
2015-10-10 19:47:11 -0400

    alphabuild_1

---
+++ UnityProject/Assets/CameraRotate.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CameraRotate : MonoBehaviour {
+
+	static int table_number = 1;
+
+	static void htmlMessage(int x) {
+		table_number = x;
+
+	}
+
+	public GameObject[] targets;
+	//public GameObject target = null;
+	public bool rotate = false;
+
+	float minDistance = 4f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (targets != null) {
+				transform.LookAt (targets[table_number].transform);
+			if(Vector3.Distance(transform.position, targets[table_number].transform.position) > minDistance)
+				transform.position = Vector3.MoveTowards(transform.position, targets[table_number].transform.position, 0.3f);
+		}
+
+		if(rotate) {
+			transform.RotateAround(targets[table_number].transform.position, Vector3.up, Time.deltaTime*20);
+		}
+	}
+}
--- js/restaurant.js
+++ js/restaurant.js
@@ -1,12 +1,21 @@
 var $page=$("<div class='popup'><div><button class='submit'>Submit</button></div></div>");
 
-$("#TableSelect li").click(function() {
-	$page.show();
-	$(".containermap").append($page);
-	console.log($(this).text());
-	
+$(".table1").click(function() {
+	console.log(1);
+	return 1;
 });
-$page.click(function(){
-	$(this).hide();
-	
+
+$(".table2").click(function() {
+	console.log(2);
+	return 2;
+});
+
+$(".table3").click(function() {
+	console.log(3);
+	return 3;
+});
+
+$(".table4").click(function() {
+	console.log(4);
+	return 4;
 });
